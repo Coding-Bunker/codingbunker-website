998a5c7623567854319c64bdf5c603f42fde8d68
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2025-05-01 18:01:57 +0200

    feat: integrate jsonwebtoken for JWT generation in MeetupApiClient

--- src/apiClient/MeetupApiClient.ts
+++ src/apiClient/MeetupApiClient.ts
@@ -1,10 +1,29 @@
 import axios from "axios"
+import jwt from 'jsonwebtoken'
 
 
 const MeetupApiClient = () =>{
 
-    async function fetchEvents(groupName : string, accessToken : string) {
+
+    function generateJWT() {
+        const now = Math.floor(Date.now() / 1000);
+        const payload = {
+        iss: process.env.MEETUP_CLIENT_ID,
+        aud: 'api.meetup.com',
+        iat: now,
+        exp: now + 60 * 5 // valido 5 minuti
+        };
+    
+        const token = jwt.sign(payload, process.env.MEETUP_PRIVATE_KEY, {
+        algorithm: 'RS256' // o HS256 se usi un secret HMAC
+        });
+    
+        return token;
+    }
+
+    async function fetchEvents(groupName : string) {
         try {
+          const accessToken = generateJWT();
           const response = await axios.get(`https://api.meetup.com/${groupName}/events`, {
             headers: {
               'Authorization': `Bearer ${accessToken}`
